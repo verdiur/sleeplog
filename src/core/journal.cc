2bfbd061e51b1c9d5a0a07431de46145ce25d8f7
verdiur <116896120+verdiur@users.noreply.github.com>
2024-07-09 15:05:16 +0200

    add journal.cc stub

---
+++ src/core/journal.cc
@@ -0,0 +1,10 @@
+#include <string>
+
+// IMPLEMENTS JOURNAL.HH
+#include <core/journal.hh>
+
+
+Journal::Journal(std::string title, std::string author):
+    m_title(title),
+    m_author(author)
+{}
